bc165416b9aaefdb1577adfc9761b9b1181e8665
Ítalo Ferreira Cardoso <39782013+Quimicall@users.noreply.github.com>
2023-04-11 15:51:54 -0300

    Update

--- controle_gastos/urls.py
+++ controle_gastos/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 if settings.DEBUG:
     urlpatterns = [
         path('admin/', admin.site.urls),
-        path('', listagem, static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)),
+        path('', listagem),
         path('nova/', nova_carta),
         path('home/', home)
     ]
