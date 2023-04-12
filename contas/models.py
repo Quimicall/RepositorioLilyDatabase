f437af23d0657123b4b1d05dc7877c4b5f285b8e
Ítalo Ferreira Cardoso <39782013+Quimicall@users.noreply.github.com>
2023-04-12 15:59:11 -0300

    update

--- contas/models.py
+++ contas/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-'''class Categoria(models.Model):
+class Categoria(models.Model):
     nomec = models.CharField(max_length=100, primary_key=True)
     dt_criacao = models.DateTimeField(auto_now_add=True)
     valor = models.DecimalField(max_digits=30, decimal_places=2)
@@ -12,8 +12,7 @@ from django.db import models
     exp = models.DecimalField(max_digits=3, decimal_places=0)
 
     def __str__(self):
-        return self.valor'''
-
+        return self.valor
 
 
 class Carta(models.Model):
