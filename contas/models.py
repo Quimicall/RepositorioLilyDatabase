48938d77c54984a91d45ccb0d1b7bc9ed2a45dff
Ítalo Ferreira Cardoso <39782013+Quimicall@users.noreply.github.com>
2023-04-12 15:01:43 -0300

    Update

--- contas/models.py
+++ contas/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Categoria(models.Model):
     nomec = models.CharField(max_length=100, primary_key=True)
     dt_criacao = models.DateTimeField(auto_now_add=True)
