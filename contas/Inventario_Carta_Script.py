63ba0803cb8f74979da421d67ee7d03ecd59bb4c
Ítalo Ferreira Cardoso <39782013+Quimicall@users.noreply.github.com>
2023-04-20 11:44:27 -0300

    Update SIGNALS
    Chegando em casa refazer o processo de registro de usuario.

--- contas/Inventario_Carta_Script.py
+++ contas/Inventario_Carta_Script.py
@@ -1,4 +1,4 @@
-from .models import Item, Inventario_Carta, User
+"""from .models import Item, Inventario_Carta, User
 
 # Esse é um exemplo de como o item pode ir para o Inventario da carta. Boa sorte Ítalo do Futuro!! :)
 
@@ -6,8 +6,21 @@ item = Item.objects.get(id='IDItem')
 inventario, created = Inventario_Carta.objects.get_or_create(usuario=User, item=Item)
 if not created:
     inventario.quantidade += 1
-inventario.save()
+inventario.save()"""
 
 # Agora um comentario mais distribuido, aqui você fez o seguinte, está puxando as váriaveis item para puxar o ID do item
 # Com isso você declarou o inventario pra ver se o item especificado. Se o objeto existir ele será retornado:
 # (inventario, False)
+
+# Roubado com sucesso (Lucas).
+
+"""n1 = int(input('Digite as ativações do talento: ')
+b = 160
+print('O buff 1 é de 160%')
+for x in range(n1 - 1):
+    r = b + 60 / (b/100)
+    b = r
+    print('O buff {x + 2} é de {r:.0f}%')"""
+
+
+# ==================================================
--- contas/signals.py
+++ contas/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver, Signal
 from contas.models import Perfil, Invent, Carteira, Carta, Item
+from django.contrib.auth.models import User
 
 
 @receiver(post_save, sender=Perfil)
@@ -10,9 +11,12 @@ def created_profile(sender, instance, created, **kwargs):
     pass
 
 
-@receiver(post_save, sender=Perfil)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+@receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
+def save_profile(sender, instance, created, **kwargs):
+    user = instance
+    if created:
+        profile = Perfil(user=user)
+        profile.save()
     pass
 
 
